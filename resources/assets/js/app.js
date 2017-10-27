6d7e04e738c3495d3069aa225f4a95e8263d183f
Miguel Angel Garcia <miguel@miguelangelgarcia.com>
2017-10-27 14:46:03 -0500

    continue updating to bootstrap 4

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,38 +1,10 @@
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
-    }
-}());
-
-var Page = {
-    width : $(window).width(),
-    height : $(window).height(),
-    init : function() {
-        $('.navbar-toggler').on('click', function(e){
-            $('#sidebar').toggleClass( 'navshow' );
-            $('#content').toggleClass( 'navshow' );
-            e.preventDefault();
-        });
-    }
-};
-
-$(function() {
-    Page.init();
-});
+/* Bootstrap
+------------------------------------------------------------------------------ */
+try {
+    window.$ = window.jQuery = require('jquery');
+    window.Popper = Popper;
+    require('bootstrap');
+    // or individually as needed
+    // require('bootstrap/js/dist/util');
+    // require('bootstrap/js/dist/dropdown');
+} catch (e) {}
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js/app.min.js')
-   .sass('resources/assets/sass/app.scss', 'public/css/app.min.css');
+mix.autoload({
+    jquery: ['$', 'window.jQuery',"jQuery","window.$","jquery","window.jquery"],
+    'popper.js/dist/umd/popper.js': ['Popper']
+    })
+    .js('resources/assets/js/app.js', 'public/js/app.min.js')
+    .sass('resources/assets/sass/app.scss', 'public/css/app.min.css')
+    .sourceMaps();;
